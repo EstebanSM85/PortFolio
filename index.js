cc3c436883c4fde3a29ff39f1261c4ad303b38f8
EstebanSM85 <estebansm85@gmail.com>
2025-05-17 09:43:21 +0200

    required quitado de html para que el eventlistener del formulario funcione

--- index.js
+++ index.js
@@ -100,3 +100,18 @@ infoPersonal.redes.forEach(otro => {
 
 aside.appendChild(nuevasRedes);
 
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", (event) => {
+        const nombre = document.getElementById("nombre").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const mensaje = document.getElementById("mensaje").value.trim();
+
+        if (nombre === "" || email === "" || mensaje === "") {
+            alert("Por favor, completa todos los campos.");
+            event.preventDefault(); // Detiene el envío del formulario
+            return;
+        }
+    });
+});
