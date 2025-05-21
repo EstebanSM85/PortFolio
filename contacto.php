68b302e62e1de5cc5baf7d3bcc3f6a4a41c31749
EstebanSM85 <estebansm85@gmail.com>
2025-05-21 12:24:59 +0200

    Inplementando contacto PHP

---
+++ contacto.php
@@ -0,0 +1,25 @@
+
+
+<?php
+    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+    $conexion = mysqli_connect("localhost", "root", "", "portfolio");
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+} else {
+    echo "Conexión exitosa";
+}
+
+
+    // Aquí sigue tu código original
+    mysqli_query($conexion, "INSERT INTO contacto(nombre,email,mensaje) VALUES 
+                    ('$_POST[nombre]','$_POST[email]', '$_POST[mensaje]')")
+    or die("Problemas en el select: " . mysqli_error($conexion));
+
+    mysqli_close($conexion);
+
+    echo "El mensaje fue enviado.";
+?>
